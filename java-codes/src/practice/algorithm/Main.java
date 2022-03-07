1d5f7d809431bbcb0b3d30da29d97422134a30d5
minhee.son <minhee4735@gmail.com>
2022-03-08 05:37:09 +0900

    feat renew java-codes dir

---
+++ java-codes/src/practice/algorithm/Main.java
@@ -0,0 +1,9 @@
+package practice.algorithm;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("args = hello word");
+    }
+}
