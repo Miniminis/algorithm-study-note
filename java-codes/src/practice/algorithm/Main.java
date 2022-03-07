f9b486543ebd0dda52d786a2c6d4ad66c6131e19
minhee.son <minhee4735@gmail.com>
2022-03-08 05:11:47 +0900

    feat 알고리즘 규칙, Main 생성완료

--- java-codes/src/practice/algorithm/Main.java
+++ java-codes/src/practice/algorithm/Main.java
@@ -3,8 +3,6 @@ package practice.algorithm;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
-        System.out.println("hello, java");
+        
     }
 }
