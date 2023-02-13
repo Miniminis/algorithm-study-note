450809ab7527bf88778d4a70f888e2b8a4d0e018
minhee.son <minhee4735@gmail.com>
2023-02-13 13:44:45 +0900

    [bj-2420] 사파리월드

---
+++ java-codes/src/practice/algorithm/implementation/Bj2420.java
@@ -0,0 +1,21 @@
+package practice.algorithm.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/2420
+ * 사파리월드
+ * */
+public class Bj2420 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        System.out.println(Math.abs(a - b));
+    }
+}
